1c2b5b1e3668f0f2af235c813984d889442d80db
marvinkn <marvin.nyingi97@gmail.com>
2024-07-01 15:16:27 +0300

    custom authentication provider

--- spring.security/spring.security/src/main/java/dev/marvin/config/CustomAuthenticationProvider.java
+++ spring.security/spring.security/src/main/java/dev/marvin/config/CustomAuthenticationProvider.java
@@ -1,20 +1,38 @@
 package dev.marvin.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         var username = authentication.getName();
         var password = authentication.getCredentials().toString();
+
+//        var user = userDetailsService.loadUserByUsername(username);
+//
+//        if(passwordEncoder.matches(password, user.getPassword())){
+//            return new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword(), user.getAuthorities());
+//        }else{
+//            throw new BadCredentialsException("invalid credentials");
+//        }
 
         if(username.equals("admin") && password.equals("password")){
             return new UsernamePasswordAuthenticationToken(username, password, List.of());
@@ -25,8 +43,6 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 
     @Override
     public boolean supports(Class<?> authentication) {
-        return UsernamePasswordAuthenticationToken
-                .class
-                .isAssignableFrom(authentication);
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
     }
 }
--- spring.security/spring.security/src/main/java/dev/marvin/config/SecurityConfig.java
+++ spring.security/spring.security/src/main/java/dev/marvin/config/SecurityConfig.java
@@ -4,7 +4,9 @@ import dev.marvin.filter.AuthenticationLoggingFilter;
 import dev.marvin.filter.RequestValidationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
@@ -29,4 +31,10 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
+
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+//        return authenticationConfiguration.getAuthenticationManager();
+//    }
+
 }
