f73df54bd8c8ae08caf6450deb8cad1b6bff573a
Marvin Nyingi <nyingi.marvin@outlook.com>
2024-09-18 22:20:31 +0300

    9/18/2024

---
+++ implementing-authentication/src/main/java/dev/marvin/ImplementingAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package dev.marvin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplementingAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImplementingAuthenticationApplication.class, args);
+	}
+
+}
