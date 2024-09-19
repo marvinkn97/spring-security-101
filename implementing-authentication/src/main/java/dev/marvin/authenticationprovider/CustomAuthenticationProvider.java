6e2b0863afdcff9647a3887718d6ebcc8b8d8bae
Marvin Nyingi <nyingi.marvin@outlook.com>
2024-09-19 15:50:42 +0300

    9/19/2024

--- implementing-authentication/src/main/java/dev/marvin/authenticationprovider/CustomAuthenticationProvider.java
+++ implementing-authentication/src/main/java/dev/marvin/authenticationprovider/CustomAuthenticationProvider.java
@@ -5,10 +5,24 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
 
+/**
+ * we create custom authentication providers given instances our application
+ * does not rely on the usual username & password authentication
+ * for example: biometrics login, otp sms etc
+ */
+
+
 @Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        if (username.equals("Marvin") && password.equals("password")) {
+        }
+
         return null;
     }
 
