88c136419523a1e3e89b660dfafd16d2d50c928e
Marvin Nyingi <nyingi.marvin@outlook.com>
2025-01-02 00:36:31 +0300

    oauth2 resource server with keycloak

---
+++ oauth2-client-github/src/main/java/dev/marvin/controller/SecureController.java
@@ -0,0 +1,15 @@
+package dev.marvin.controller;
+
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecureController {
+
+    @GetMapping("/")
+    public String secure(OAuth2AuthenticationToken oAuth2AuthenticationToken){
+        return "secure" + oAuth2AuthenticationToken;
+
+    }
+}
